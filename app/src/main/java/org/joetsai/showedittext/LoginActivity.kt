b854dddfb4340880724e3ee8b4508ce5a6ee2819
Joe Tsai <tsuning2000@gmail.com>
2017-11-28 15:05:44 +0800

    Initial commit

---
+++ app/src/main/java/org/joetsai/showedittext/LoginActivity.kt
@@ -0,0 +1,15 @@
+package org.joetsai.showedittext
+
+import android.content.Intent
+import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+import kotlinx.android.synthetic.main.activity_login.*
+
+class LoginActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_login)
+        buttonLogin.setOnClickListener { startActivity(Intent(this, MainActivity::class.java)) }
+    }
+}
